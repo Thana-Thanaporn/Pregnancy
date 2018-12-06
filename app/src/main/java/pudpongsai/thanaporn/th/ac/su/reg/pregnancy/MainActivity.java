03824a8d30eccf6e9f317d6b1666a98024d204a3
thanaporn <toth.naja@gmail.com>
2018-12-06 11:04:01 +0700

    Initial commit

---
+++ app/src/main/java/pudpongsai/thanaporn/th/ac/su/reg/pregnancy/MainActivity.java
@@ -0,0 +1,13 @@
+package pudpongsai.thanaporn.th.ac.su.reg.pregnancy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
